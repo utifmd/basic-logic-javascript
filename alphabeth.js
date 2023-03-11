1adf010f9607469e19502a77c1096414664f0752
Utif Milkedori <utifmd@gmail.com>
2023-03-11 16:37:05 +0700

    letter positions

--- alphabeth.js
+++ alphabeth.js
@@ -1,6 +1,17 @@
+const request = 'The sunset sets at twelve o\' clock.';
+// const response = _operateLetterPosition(request)
 const response = _operate()
 console.log(response)
 
+function _operateLetterPosition(params) {
+    let output = []
+    const alphabeth = _operate()
+    const data = params.replace(/[^a-zA-Z]/gm, "").toLowerCase()
+    for(let i = 0; i < data.length; i++){
+        output.push(alphabeth.indexOf(data[i]) +1)
+    }
+    return output.join(" ");
+}
 function _operate() {
     let output = ""
     for (
