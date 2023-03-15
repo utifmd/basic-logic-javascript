84bbe4f0cb7b2be026c1c9085841806a880be31b
Utif Milkedori <utifmd@gmail.com>
2023-03-15 11:47:02 +0700

    random by number

---
+++ randomPosition.js
@@ -0,0 +1,16 @@
+const request = 7
+const response = _operate(request)
+
+console.log(response)
+
+function _operate(params) {
+    let output = '', threshold = params
+    for (let i = 100; i < 900; i++) {
+        if (i % 10 !== params) continue
+        if (threshold < 0) break
+
+        output += `${i} `
+        threshold--
+    }
+    return output
+}
